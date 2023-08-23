e913a3be05475df1ef71199d4ce227f551918adb
Oluwasegun Atolagbe <58482668+AtVict0r@users.noreply.github.com>
2023-08-23 17:14:36 -0400

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,145 @@
+﻿namespace HoroscopeReading
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.verifyUsernameButton = new System.Windows.Forms.Button();
+            this.verifyNameLabel = new System.Windows.Forms.Label();
+            this.yesButton = new System.Windows.Forms.Button();
+            this.noButton = new System.Windows.Forms.Button();
+            this.usernameLabel = new System.Windows.Forms.Label();
+            this.usernameTextBox = new System.Windows.Forms.TextBox();
+            this.invalidNameLabel = new System.Windows.Forms.Label();
+            this.getReadingLabel = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // verifyUsernameButton
+            // 
+            this.verifyUsernameButton.Location = new System.Drawing.Point(167, 174);
+            this.verifyUsernameButton.Name = "verifyUsernameButton";
+            this.verifyUsernameButton.Size = new System.Drawing.Size(75, 23);
+            this.verifyUsernameButton.TabIndex = 0;
+            this.verifyUsernameButton.Text = "Verify";
+            this.verifyUsernameButton.UseVisualStyleBackColor = true;
+            this.verifyUsernameButton.Click += new System.EventHandler(this.verifyUsernameButton_Click);
+            // 
+            // verifyNameLabel
+            // 
+            this.verifyNameLabel.AutoSize = true;
+            this.verifyNameLabel.Location = new System.Drawing.Point(48, 216);
+            this.verifyNameLabel.Name = "verifyNameLabel";
+            this.verifyNameLabel.Size = new System.Drawing.Size(0, 13);
+            this.verifyNameLabel.TabIndex = 1;
+            // 
+            // yesButton
+            // 
+            this.yesButton.Location = new System.Drawing.Point(51, 253);
+            this.yesButton.Name = "yesButton";
+            this.yesButton.Size = new System.Drawing.Size(75, 23);
+            this.yesButton.TabIndex = 2;
+            this.yesButton.Text = "Yes";
+            this.yesButton.UseVisualStyleBackColor = true;
+            this.yesButton.Visible = false;
+            this.yesButton.Click += new System.EventHandler(this.yesButton_Click);
+            // 
+            // noButton
+            // 
+            this.noButton.Location = new System.Drawing.Point(167, 253);
+            this.noButton.Name = "noButton";
+            this.noButton.Size = new System.Drawing.Size(75, 23);
+            this.noButton.TabIndex = 3;
+            this.noButton.Text = "No";
+            this.noButton.UseVisualStyleBackColor = true;
+            this.noButton.Visible = false;
+            this.noButton.Click += new System.EventHandler(this.noButton_Click);
+            // 
+            // usernameLabel
+            // 
+            this.usernameLabel.AutoSize = true;
+            this.usernameLabel.Location = new System.Drawing.Point(48, 83);
+            this.usernameLabel.Name = "usernameLabel";
+            this.usernameLabel.Size = new System.Drawing.Size(88, 13);
+            this.usernameLabel.TabIndex = 1;
+            this.usernameLabel.Text = "Enter Your Name";
+            // 
+            // usernameTextBox
+            // 
+            this.usernameTextBox.Location = new System.Drawing.Point(142, 83);
+            this.usernameTextBox.Name = "usernameTextBox";
+            this.usernameTextBox.Size = new System.Drawing.Size(100, 20);
+            this.usernameTextBox.TabIndex = 4;
+            // 
+            // invalidNameLabel
+            // 
+            this.invalidNameLabel.AutoSize = true;
+            this.invalidNameLabel.Location = new System.Drawing.Point(48, 118);
+            this.invalidNameLabel.Name = "invalidNameLabel";
+            this.invalidNameLabel.Size = new System.Drawing.Size(0, 13);
+            this.invalidNameLabel.TabIndex = 1;
+            // 
+            // getReadingLabel
+            // 
+            this.getReadingLabel.AutoSize = true;
+            this.getReadingLabel.Location = new System.Drawing.Point(48, 290);
+            this.getReadingLabel.Name = "getReadingLabel";
+            this.getReadingLabel.Size = new System.Drawing.Size(0, 13);
+            this.getReadingLabel.TabIndex = 5;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.getReadingLabel);
+            this.Controls.Add(this.usernameTextBox);
+            this.Controls.Add(this.noButton);
+            this.Controls.Add(this.yesButton);
+            this.Controls.Add(this.invalidNameLabel);
+            this.Controls.Add(this.usernameLabel);
+            this.Controls.Add(this.verifyNameLabel);
+            this.Controls.Add(this.verifyUsernameButton);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button verifyUsernameButton;
+        private System.Windows.Forms.Label verifyNameLabel;
+        private System.Windows.Forms.Button yesButton;
+        private System.Windows.Forms.Button noButton;
+        private System.Windows.Forms.Label usernameLabel;
+        private System.Windows.Forms.TextBox usernameTextBox;
+        private System.Windows.Forms.Label invalidNameLabel;
+        private System.Windows.Forms.Label getReadingLabel;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace HoroscopeReading
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        char firstLetter = ' ';
+        int nameLength = 0;
+
+        private void verifyUsernameButton_Click(object sender, EventArgs e)
+        {
+            invalidNameLabel.Text = String.Empty;
+            verifyNameLabel.Text = String.Empty;
+            yesButton.Visible = false;
+            noButton.Visible = false;
+            getReadingLabel.Text = String.Empty;
+
+            string userName = usernameTextBox.Text;
+            
+            if (userName.All(Char.IsLetter) && userName != "")
+            {
+                verifyNameLabel.Text = $"Is {userName} the name you entered?";
+                yesButton.Visible = true;
+                noButton.Visible = true;
+                firstLetter = userName[0];
+                nameLength = userName.Length;
+            }
+            else 
+            {
+                invalidNameLabel.Text = "Your name should only contain alphabets.\nSpecial characters and numbers are not allowed!.\nTry Again!!";
+                usernameTextBox.Text = String.Empty;
+            }
+        }
+
+        private void noButton_Click(object sender, EventArgs e)
+        {
+            invalidNameLabel.Text = String.Empty;
+            verifyNameLabel.Text = "Enter your name again.";
+            yesButton.Visible = false;
+            noButton.Visible = false;
+            getReadingLabel.Text = String.Empty;
+            firstLetter = ' ';
+            nameLength = 0;
+            usernameTextBox.Text = String.Empty;
+        }
+
+        private void yesButton_Click(object sender, EventArgs e)
+        {
+            invalidNameLabel.Text = String.Empty;
+            verifyNameLabel.Text = String.Empty;
+            getReadingLabel.Text = String.Empty;
+            usernameTextBox.Text = String.Empty;
+
+            Random rnd = new Random();
+            int num = rnd.Next(Math.Min((int)firstLetter, nameLength), Math.Min(Math.Max((int)firstLetter, nameLength), 26));
+
+            string[] message = new string[] {"A friend is hidding something from you.",
+                                             "Start something new today.",
+                                             "Revisit the past.",
+                                             "Finish an uncompleted project.",
+                                             "Take sometime today to appreciate your surrounding.",
+                                             "Look at it from a different perspective.",
+                                             "Look towards the future.",
+                                             "You still have time.",
+                                             "You can always try again.",
+                                             "Don't be afraid of failure.",
+                                             "It is never too late.",
+                                             "It is okay to move.",
+                                             "Get out of your comfort zone.",
+                                             "Be flexible.",
+                                             "You are not the center of the universe.",
+                                             "Put in more effort.",
+                                             "The road is just up ahead.",
+                                             "Have faith.",
+                                             "It will work out.",
+                                             "A bit of positivity goes a long way.",
+                                             "Slow and steady wins the race.",
+                                             "Be calm and confident.",
+                                             "Time is on your side.",
+                                             "Explore new things today.",
+                                             "Listen to the advice you get today",
+                                             "You will influence someone today"};
+
+            getReadingLabel.Text = "Getting reading...\n..." + message[num];
+        }
+    }
+}
